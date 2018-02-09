bb1eeac14b12ab7f28bb145df8148588064002e5
penglei <yupengniaosanshi@gmail.com>
2018-02-09 10:36:09 +0800

    添加示例代码

---
+++ src/main/java/CountingSheep.java
@@ -0,0 +1,27 @@
+/**
+ * CountingSheep
+ * <p/>
+ * Counting sheep
+ *
+ * @author Brian Goetz and Tim Peierls
+ */
+public class CountingSheep {
+    volatile boolean asleep;
+
+    void tryToSleep() {
+        while (!asleep)
+            countSomeSheep();
+    }
+
+    void countSomeSheep() {
+        // One, two, three...
+    }
+}
+
+
+
+
+
+
+
+
---
+++ src/main/java/Sequence.java
@@ -0,0 +1,16 @@
+import net.jcip.annotations.*;
+
+/**
+ * Sequence
+ *
+ * @author Brian Goetz and Tim Peierls
+ */
+
+@ThreadSafe
+public class Sequence {
+    @GuardedBy("this") private int nextValue;
+
+    public synchronized int getNext() {
+        return nextValue++;
+    }
+}
